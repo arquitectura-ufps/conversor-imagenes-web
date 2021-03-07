1e617c02030131a4b8d80203d696839e630fa094
leabiv <leabiv@gmail.com>
2021-03-06 23:26:02 -0500

    Prueba de metodo

--- src/main/java/co/edu/ufps/web/ControllerRest/IndexController.java
+++ src/main/java/co/edu/ufps/web/ControllerRest/IndexController.java
@@ -15,9 +15,24 @@ public class IndexController {
     //@RequestMapping(value = "", method = RequestMethod.POST, headers="Content-Type=multipart/form-data")
     //@PostMapping("/converter")
     @GetMapping("/converter")
-    public String converter() {
+    public String converter(){
+//    public String converter(String inputImagePath, String outputImagePath, String formatName) {
         serviceConverter.run();
-        return "Good day";
+
+//        FileInputStream inputStream = new FileInputStream(inputImagePath);
+//        FileOutputStream outputStream = new FileOutputStream(outputImagePath);
+        // reads input image from file
+//        BufferedImage inputImage = ImageIO.read(inputStream);
+
+        // writes to the output image in specified format
+//        boolean result = ImageIO.write(inputImage, formatName, outputStream);
+
+        // needs to close the streams
+//        outputStream.close();
+//        inputStream.close();
+
+//        return result;
+          return "Imagen";
     }
 
 }
